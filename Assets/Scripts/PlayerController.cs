0b3b777dfca8140d3c31edfd68859b53c0c0d42b
Marazair <nick1022m@gmail.com>
2019-06-05 19:25:48 -0400

    Player Movement, Game Manager, and Floor Tiles
    Created a basic character object that can move around the screen freely. Created a Game Manager which will persist between scenes and keep track of lives. Created a tilemap for the floor tiles.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Debug.Log(horizontal);
+        Debug.Log(vertical);
+
+        Vector2 position = transform.position;
+        position.x += horizontal * Time.deltaTime * 3.0f;
+        position.y += vertical * Time.deltaTime * 3.0f;
+        
+        transform.position = position;
+    }
+}
