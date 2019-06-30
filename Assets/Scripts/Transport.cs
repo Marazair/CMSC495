aa62d50691e379b502197070add74ee1e045b506
marknielson39 <51351070+marknielson39@users.noreply.github.com>
2019-06-30 14:23:20 -0700

    Add files via upload

---
+++ Assets/Scripts/Transport.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Transport : MonoBehaviour {
+        
+    public Transform teleportTarget;
+    public GameObject Player;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {    
+        Player.transform.position = teleportTarget.transform.position;
+    }
+}
---
+++ Assets/Scripts/respawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+      
+    public Transform Player;
+    public Transform respawnPoint;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Player.transform.position = respawnPoint.transform.position;
+    }    
+}
