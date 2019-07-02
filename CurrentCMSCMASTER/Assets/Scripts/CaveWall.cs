c986ee2bf2a45bca1162f4105c06b47a35e572d4
gallaghercareer <gallaghercareer@gmail.com>
2019-07-02 02:21:00 -0400

    upload JG

---
+++ CurrentCMSCMASTER/Assets/Scripts/CaveWall.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveWall : MonoBehaviour, Obstacle {
+    
+    public void OnPlayerInteract(PlayerController player) {
+        if (player.isMoving()) {
+            return;
+        }
+    }
+}
---
+++ CurrentCMSCMASTER/Assets/Scripts/GridMover.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GridMover : MonoBehaviour
+{
+    public float moveTime = .1f;
+    public LayerMask blockingLayer;
+
+    private Collider2D objectCollider;
+    private Rigidbody2D rb2D;
+    private float inverseMoveTime;
+    private bool moving;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        objectCollider = GetComponent<Collider2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        inverseMoveTime = 1f / moveTime;
+        moving = false;
+    }
+
+    protected IEnumerator SmoothMovement (Vector3 end) 
+    {
+        float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+
+        while (sqrRemainingDistance > float.Epsilon) 
+        {
+            Vector3 newPosition = Vector3.MoveTowards (rb2D.position, end, inverseMoveTime * Time.deltaTime);
+            rb2D.MovePosition(newPosition);
+            sqrRemainingDistance = (transform.position - end).sqrMagnitude;
+            yield return null;
+        }
+
+        moving = false;
+    }
+
+    protected bool Move (int xDir, int yDir, out RaycastHit2D hit) 
+    {
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(xDir, yDir);
+        
+        objectCollider.enabled = false;
+        hit = Physics2D.Linecast(start, end, blockingLayer);
+        objectCollider.enabled = true;
+
+        if (hit.transform == null)
+        {
+            if(moving == false) 
+            {
+                moving = true;
+                StartCoroutine(SmoothMovement (end));
+            }
+            return true;
+        }
+
+        return false;
+    }
+    protected virtual void AttemptMove <T> (int xDir, int yDir) 
+        where T : Obstacle
+    {
+        RaycastHit2D hit;
+        bool canMove = Move (xDir, yDir, out hit);
+
+        if (hit.transform == null)
+            return;
+
+        T hitComponent = hit.transform.GetComponent<T>();
+
+        if (!canMove && hitComponent != null)
+            OnCantMove(hitComponent);
+    }
+
+    public bool isMoving() {
+        return moving;
+    }
+    protected abstract void OnCantMove<T>(T component)
+        where T:Obstacle;
+}
