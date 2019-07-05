cf60fb45677c618474d69c1af6a0f9d836b6e915
Marazair <nick1022m@gmail.com>
2019-07-05 16:21:12 -0700

    Readded objects

---
+++ Assets/Scripts/Gem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gem : MonoBehaviour
+{
+
+    private GameObject specialWall;
+
+    private static int gemsCollected = 0;
+    // Start is called before the    first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            gemsCollected = gemsCollected + 1;
+          
+        }
+        if (gemsCollected == 3)
+        {
+            specialWall = GameObject.FindGameObjectWithTag("SpecialWall");
+            Destroy(specialWall);
+
+        }
+
+    }
+
+    public static void ResetGems() {
+        gemsCollected = 0;
+    }
+}
